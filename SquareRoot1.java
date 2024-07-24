50e3e1656e653c714b8de48d8e176f5041d386e5
Sheshank <167999396+Sheshank22@users.noreply.github.com>
2024-07-24 23:29:27 +0530

    Find the Square Root of a number
    Finding Square Root of a number using built-in function

---
+++ SquareRoot1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SquareRoot1{
+  public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter a number:");
+      int num = sc.nextInt();
+      System.out.println(Math.sqrt(num));
+      
+    }
+}
